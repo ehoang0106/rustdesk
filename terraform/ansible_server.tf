918d78be16b1ee71cebcc807b22fa88e9b0b057c
Khoa Hoang <khoahoang1698@gmail.com>
2025-02-19 09:18:28 -0800

    ansible

--- terraform/ansible_server.tf
+++ terraform/ansible_server.tf
@@ -6,3 +6,63 @@ resource "aws_subnet" "ansible_subnet" {
   }
 }
 
+#allocate eip for ansible server
+resource "aws_eip" "ansible_eip" {
+  instance = aws_instance.ansible_server.id
+}
+
+resource "aws_security_group" "ansible_sg" {
+  vpc_id = aws_vpc.remote_vpc.id
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ansible_sg"
+  }
+}
+
+resource "aws_instance" "ansible_server" {
+  ami                    = "ami-07d2649d67dbe8900" #ubuntu
+  instance_type = var.instance_type
+  subnet_id = aws_subnet.ansible_subnet.id
+  security_groups = [aws_security_group.ansible_sg.id]
+  key_name = "ansible-kp"
+  root_block_device {
+    volume_size = 8
+    delete_on_termination = true
+  }
+
+  tags = {
+    Name = "ansible-server"
+  }
+
+  associate_public_ip_address = true
+}
+
+output "ansible_ec2_name" {
+  value = aws_instance.ansible_server.tags.Name
+}
+
+output "ansible_subnet_name" {
+  value = aws_subnet.ansible_subnet.tags.Name
+}
+
+output "ansible_security_group" {
+  value = aws_security_group.ansible_sg.tags.Name
+}
+
+output "ansible_instance_ip" {
+  value = aws_instance.ansible_server.public_ip
+}
